e7efa81ebdd02f8733dab9cbf25efbc3fa62ed41
marquisalexander <marquis.alexander@outlook.com>
2024-10-10 11:32:46 -0300

    finalizando CRUD da api

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -25,10 +26,16 @@ class LivroController {
 	}
 
 	static async cadastrarLivro(req, res) {
+		const novoLivro = req.body;
 		try {
-			const novoLivro = await livro.create(req.body);
+			const autorEncontrado = await autor.findById(novoLivro.autor);
+			const livroCompleto = {
+				...novoLivro,
+				autor: { ...autorEncontrado._doc },
+			};
+			const livroCriado = await livro.create(livroCompleto);
 			res.status(201).json({
-				livro: novoLivro,
+				livro: livroCriado,
 				message: "livro cadastrado com sucesso",
 			});
 		} catch (error) {
@@ -59,6 +66,16 @@ class LivroController {
 			res
 				.status(500)
 				.json({ message: `${error.message} - falha ao deletar livro` });
+		}
+	}
+
+	static async listarLivrosPorEditora(req, res) {
+		const editora = req.query.editora;
+		try {
+			const livrosPorEditora = await livro.find({ editora: editora });
+			res.status(200).json({ livrosPorEditora });
+		} catch (error) {
+			res.status(500).json({ message: `Falha na busca` });
 		}
 	}
 }
